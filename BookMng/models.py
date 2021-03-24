f0eae13ef42166a9665900fb765f0a42c1201d49
Shrikant Sawarkar <shrikantsawarkar96@gmail.com>
2021-03-25 01:44:10 +0530

    added seller model

--- BookMng/models.py
+++ BookMng/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Seller(models.Model):
+    email = models.EmailField( max_length=100)
+    usertype = models.CharField( max_length=50)
+    password = models.CharField(max_length=50)
+    def __str__(self):
+        return self.email
